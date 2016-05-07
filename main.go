db537941f408b6595d339f19f2e86cfc9f002b0c
Andrews Medina <andrewsmedina@gmail.com>
2016-05-06 22:40:22 -0300

    add a code to check if the object is a handler

--- main.go
+++ main.go
@@ -19,12 +19,31 @@ func apiLoader() (*loader.Program, error) {
 	return ldr.Load()
 }
 
+func isHandler(object *ast.Object) bool {
+	params := object.Decl.(*ast.FuncDecl).Type.Params.List
+	if len(params) < 2 {
+		return false
+	}
+	for _, param := range params {
+		t, ok := param.Type.(*ast.SelectorExpr)
+		if ok {
+			if t.Sel.Name == "ResponseWriter" {
+				return true
+			}
+		}
+	}
+	return false
+}
+
 func parse(prog *loader.Program) error {
 	files := prog.Imported["github.com/tsuru/tsuru/api"].Files
 	for _, f := range files {
 		for _, object := range f.Scope.Objects {
 			if object.Kind == ast.Fun && object.Name == "serviceList" {
-				fmt.Printf("handler: %s - %#v\n", object.Name, object.Decl.(*ast.FuncDecl).Doc)
+				ok := isHandler(object)
+				if !ok {
+					continue
+				}
 				commentGroup := object.Decl.(*ast.FuncDecl).Doc
 				for _, comment := range commentGroup.List {
 					fmt.Println(comment.Text)
